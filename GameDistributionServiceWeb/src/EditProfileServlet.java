f5bab83c4101051586126e9643491e00e8ee3097
gavan2 <113039560+gavan2@users.noreply.github.com>
2022-10-12 10:18:06 +0700

    hoan thien them

---
+++ GameDistributionServiceWeb/src/EditProfileServlet.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dal.userDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.User;
+
+/**
+ *
+ * @author ADMIN
+ */
+@WebServlet(name = "EditProfileServlet", urlPatterns = {"/editprofile"})
+public class EditProfileServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EditProfileServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EditProfileServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int Id = Integer.parseInt(request.getParameter("UserID"));
+
+        User user = new userDAO().findUserByID(Id);
+        request.setAttribute("user", user);
+        request.getRequestDispatcher("EditProfile.jsp").forward(request, response);
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            int id = Integer.parseInt(request.getParameter("id"));
+            String nickname = request.getParameter("nickname");
+            String country = request.getParameter("country");
+            String email = request.getParameter("email");
+            String avatar = request.getParameter("avatar");
+            String decription = request.getParameter("decription");
+            boolean isPrivate = Boolean.parseBoolean(request.getParameter("private"));
+            User u = new User(id, nickname, country, email, avatar, decription, isPrivate);
+            if (new userDAO().updateProfileUser(u) > 0) {
+                HttpSession session = request.getSession();
+                session.removeAttribute("userlogin");                
+                session.setAttribute("userlogin", u);               
+                response.sendRedirect("profile?UserID=" + id);
+            }
+        } catch (NumberFormatException e) {
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
